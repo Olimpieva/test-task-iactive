154a788ea25d3ef45de51be3961e369402a524a7
Olimpieva Natalya <ukurennyi.koshak@gmail.com>
2022-05-05 18:07:56 +0300

    styles: Message, MessageList, add fonts, change icons

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { ReactComponent as Avatar } from '../../images/avatar.svg';
 import { ReactComponent as LikeIcon } from '../../images/icons/like-icon.svg';
@@ -8,46 +8,87 @@ import { ReactComponent as SendMessageIcon } from '../../images/icons/send-messa
 import { ReactComponent as HideMessageIcon } from '../../images/icons/hide-message-icon.svg';
 
 import './Message.css';
+import { handleDate } from '../../utils/constants';
 
 function Message({ item, isLiked, likeMessageHandler }) {
 
     const { author, content, date, attachments } = item;
+    const currentDate = handleDate(date);
+
+    const [isFullContentShown, setIsFullContentShown] = useState(false);
 
     const multimedia = attachments.reduce((acc, item) => {
         acc[item.type] = item.url
         return acc;
     }, {});
 
-
     return (
         <div className="message">
-            <div>
-                <Avatar />
-                <span>{date}</span>
-            </div>
-            <div>
-                <h2>{author}</h2>
-                <div>
-                    <div>
-                        <p>{content}</p>
+
+            <div className="message__container">
+
+                <div className="message__left-section">
+                    <Avatar />
+                    <span>{currentDate.time}</span>
+                </div>
+
+                <div className="message__right-section">
+
+                    <div className="message__header">
+
                         <div>
-                            <SendMessageIcon />
-                            <HideMessageIcon />
-                            <SettingsIcon />
-                            <button onClick={() => likeMessageHandler(item.id)}>
-                                {isLiked ? <LikeIconActive /> : <LikeIcon />}
-                            </button>
+                            <h2 className="message__author">{author}</h2>
+                            <span className="message__comment">Текст поста в соц. сетях если это комментарий</span>
+                        </div>
+
+                        <div className="message__menu menu">
+
+                            <div className="menu__i-dont-know-what-its-for i-dont-know-what-its-for">
+                                <button className="button i-dont-know-what-its-for__button">Левый</button>
+                                <button className="button i-dont-know-what-its-for__button">Центр</button>
+                                <button className="button i-dont-know-what-its-for__button">Правый</button>
+                            </div>
+
+                            <div className="menu__actions actions">
+                                <button className="button actions__button" onClick={() => console.log('Send Message')}>
+                                    <SendMessageIcon />
+                                </button>
+                                <button className="button actions__button" onClick={() => console.log('Hide Message')}>
+                                    <HideMessageIcon />
+                                </button>
+                                <button className="button actions__button" onClick={() => console.log('Open Settings')}>
+                                    <SettingsIcon />
+                                </button>
+                                <button className="button actions__button" onClick={() => likeMessageHandler(item.id)}>
+                                    {isLiked ? <LikeIconActive /> : <LikeIcon />}
+                                </button>
+                            </div>
 
                         </div>
                     </div>
 
-                    {multimedia.image && <img src={multimedia.image} width="400" height="200" alt="content" />}
-                    {multimedia.video &&
-                        <video width="400" height="200" controls >
-                            <source src={multimedia.video} type="video/mp4" />
-                        </video>
-                    }
+                    <div className="message-content">
+                        <p className={`message-content__text ${content.length > 400 && !isFullContentShown && 'message-content__text_hidden'}`}>{content}</p>
+                        {content.length > 400 &&
+                            <button className="button message-content__button" onClick={() => setIsFullContentShown(prevState => !prevState)}>
+                                {isFullContentShown ? 'Скрыть' : 'Далее'}
+                            </button>
+                        }
+                        {multimedia.image &&
+                            <img src={multimedia.image} width="400" height="200" alt="content" />
+                        }
+                        {multimedia.video &&
+                            <video className="message-content__video" controls >
+                                <source src={multimedia.video} type="video/mp4" />
+                            </video>
+                        }
+                    </div>
                 </div>
+            </div>
+
+            <div className="hashtag-container">
+                <span className="hashtag">#New</span>
+                <span className="hashtag">#Expert</span>
             </div>
         </div>
     );
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -20,3 +20,20 @@ export const createFormData = (data) => {
 
     return formData;
 };
+
+export const handleDate = (dateToHandle) => {
+    const now = new Date();
+    const date = new Date(dateToHandle);
+
+    const handleDateDay = date.getDate() > 9 ? date.getDate() : '0' + date.getDate();
+    const handleDateMonth = (date.getMonth() + 1) > 9 ? (date.getMonth() + 1) : '0' + (date.getMonth() + 1);
+    const handleDateYear = date.getFullYear();
+    const handleDateHours = date.getHours() > 9 ? date.getHours() : '0' + date.getHours();
+    const handleDateMinutes = date.getMinutes() > 9 ? date.getMinutes() : '0' + date.getMinutes();
+
+    return {
+        time: `${handleDateHours}:${handleDateMinutes}`,
+        date: `${handleDateDay}-${handleDateMonth}-${handleDateYear}`,
+        isToday: now.getFullYear() === handleDateYear && now.getMonth() === date.getMonth() && now.getDate() === date.getDate(),
+    }
+}
